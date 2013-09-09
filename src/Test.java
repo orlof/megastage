e970d9a3dec2280ffbabc2aa6c99869b1a454731
Teppo Koskinen <teppo.koskinen@kone.com>
2013-09-09 12:54:00 +0300

    fix for admiral keyboard input

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,31 @@
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        BufferedImage img = new BufferedImage(128, 112, BufferedImage.TYPE_INT_ARGB);
-        int[] pixels = ((DataBufferInt) img.getRaster().getDataBuffer()).getData();
-        System.out.println("pixels.length = " + pixels.length);
+        JFrame frame = new JFrame();
+        frame.setSize(200,200);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+        KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        manager.addKeyEventDispatcher(new KeyEventDispatcher() {
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent e) {
+                System.out.println("e.getKeyChar() = " + ((int) e.getKeyChar()));
+                System.out.println("e.getKeyCode() = " + ((int) e.getKeyCode()));
+                return true;
+            }
+        });
+
+        while(true) {
+            Thread.sleep(1000);
+        }
     }
 }
 
--- src/org/megastage/systems/ClientKeyboardSystem.java
+++ src/org/megastage/systems/ClientKeyboardSystem.java
@@ -1,76 +1,69 @@
 package org.megastage.systems;
 
-import com.artemis.ComponentMapper;
-import com.artemis.Entity;
-import com.artemis.annotations.Mapper;
 import com.artemis.systems.VoidEntitySystem;
-import org.megastage.components.client.VirtualMonitorView;
-import org.megastage.util.Globals;
-import org.megastage.util.Network;
-import org.megastage.util.NetworkListener;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.nio.ByteBuffer;
-import java.util.HashMap;
 
 public class ClientKeyboardSystem extends VoidEntitySystem {
     public ClientKeyboardSystem() {
         super();
 
-        // TODO keeping button down should never produce repeated key_typed or pressed events
+        // only one pressed and typed event is generated without release (no key repeat)
         KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
         manager.addKeyEventDispatcher(new KeyEventDispatcher() {
-            boolean[] downChar = new boolean[256];
-            boolean[] downCode = new boolean[256];
-            boolean[] downChar2 = new boolean[256];
+            boolean[] keyCharPressed = new boolean[256];
+            boolean[] keyCharTyped = new boolean[256];
+            boolean[] keyCodePressed = new boolean[256];
+
             @Override
             public boolean dispatchKeyEvent(KeyEvent e) {
                 System.out.println("ClientKeyboardSystem.dispatchKeyEvent");
                 if (e.getID() == KeyEvent.KEY_PRESSED) {
-                    if(e.getKeyChar() == 65535) {
-                        if(!downCode[e.getKeyCode()]) {
-                            downCode[e.getKeyCode()] = true;
-                            System.out.println("PRESS e.getKeyCode() = " + (int) e.getKeyCode());
-                            world.getSystem(ClientNetworkSystem.class).sendKeyPressed(e.getKeyCode());
+                    if(e.getKeyChar() == KeyEvent.CHAR_UNDEFINED) {
+                        int keyCode = e.getKeyCode();
+                        if(keyCode < 256 && !keyCodePressed[keyCode]) {
+                            System.out.println("PRESSED Code = " + e.getKeyCode());
+                            keyCodePressed[keyCode] = true;
+                            world.getSystem(ClientNetworkSystem.class).sendKeyPressed(keyCode);
                         }
                         return true;
                     }
 
-                    if(!downChar[e.getKeyChar()]) {
-                        downChar[e.getKeyChar()] = true;
-                        System.out.println("PRESS e.getKeyChar() = " + (int) e.getKeyChar());
+                    char keyChar = e.getKeyChar();
+                    if(keyChar < 256 && !keyCharPressed[keyChar]) {
+                        System.out.println("PRESSED Char = " + (int) e.getKeyChar());
+                        keyCharPressed[keyChar] = true;
                         world.getSystem(ClientNetworkSystem.class).sendKeyPressed(e.getKeyChar());
                     }
                     return true;
 
                 } else if (e.getID() == KeyEvent.KEY_RELEASED) {
-                    if(e.getKeyChar() == 65535) {
-                        if(downCode[e.getKeyCode()]) {
-                            downCode[e.getKeyCode()] = false;
-                            System.out.println("REL e.getKeyCode() = " + (int) e.getKeyCode());
-                            world.getSystem(ClientNetworkSystem.class).sendKeyReleased(e.getKeyCode());
+                    if(e.getKeyChar() == KeyEvent.CHAR_UNDEFINED) {
+                        int keyCode = e.getKeyCode();
+                        if(keyCode < 256 && keyCodePressed[keyCode]) {
+                            System.out.println("RELEASED Code = " + keyCode);
+                            keyCodePressed[keyCode] = false;
+                            world.getSystem(ClientNetworkSystem.class).sendKeyReleased(keyCode);
                         }
                         return true;
                     }
 
-                    if(downChar[e.getKeyChar()] || downChar2[e.getKeyChar()]) {
-                        downChar[e.getKeyChar()] = false;
-                        downChar2[e.getKeyChar()] = false;
-                        System.out.println("REL e.getKeyChar() = " + (int) e.getKeyChar());
-                        world.getSystem(ClientNetworkSystem.class).sendKeyReleased(e.getKeyChar());
+                    char keyChar = e.getKeyChar();
+                    if(keyChar < 256 && keyCharPressed[keyChar]) {
+                        System.out.println("RELEASED Char = " + (int) keyChar);
+                        keyCharPressed[keyChar] = false;
+                        keyCharTyped[keyChar] = false;
+                        world.getSystem(ClientNetworkSystem.class).sendKeyReleased(keyChar);
                     }
                     return true;
 
                 } else if (e.getID() == KeyEvent.KEY_TYPED) {
-                    if(!downChar2[e.getKeyChar()]) {
-                        downChar2[e.getKeyChar()] = true;
-                        System.out.println("TYPE e.getKeyChar() = " + (int) e.getKeyChar());
-                        world.getSystem(ClientNetworkSystem.class).sendKeyTyped(e.getKeyChar());
+                    char keyChar = e.getKeyChar();
+                    if(keyChar < 256 && !keyCharTyped[keyChar]) {
+                        System.out.println("TYPED Char = " + (int) keyChar);
+                        keyCharTyped[keyChar] = true;
+                        world.getSystem(ClientNetworkSystem.class).sendKeyTyped(keyChar);
                     }
                     return true;
                 }
