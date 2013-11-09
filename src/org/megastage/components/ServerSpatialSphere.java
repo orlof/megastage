f8f66397b717ea3e1c58b662b2e7fb7cae90fd8b
orlof <orlof@mbnet.fi>
2013-11-09 23:28:49 +0200

    random testing with system distances

---
+++ src/org/megastage/components/ServerSpatialSphere.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.megastage.components;
+
+import com.artemis.Component;
+import com.artemis.Entity;
+import com.artemis.World;
+import org.jdom2.Element;
+
+/**
+ *
+ * @author Teppo
+ */
+public class ServerSpatial extends BaseComponent {
+
+    @Override
+    public void init(World world, Entity parent, Element element) throws Exception {
+    }
+    
+}
