a0e3dd2658eaf939d0f5ca7bed8b2da27f0fbcc8
Orlof <orlof@mbnet.fi>
2014-02-26 13:52:49 +0200

    testing block picking

--- src/org/megastage/components/transfer/MonitorData.java
+++ src/org/megastage/components/transfer/MonitorData.java
@@ -2,13 +2,10 @@ package org.megastage.components.transfer;
 
 import com.artemis.Entity;
 import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.minlog.Log;
 import org.megastage.components.client.ClientVideoMemory;
 import org.megastage.components.dcpu.LEMUtil;
 import org.megastage.client.ClientGlobals;
 import org.megastage.components.BaseComponent;
-import org.megastage.protocol.Message;
-import org.megastage.util.ID;
 import org.megastage.util.RAM;
 
 public class MonitorData extends BaseComponent {
---
+++ src/org/megastage/util/CubeFinder.java
@@ -0,0 +1,76 @@
+package org.megastage.util;
+
+import com.badlogic.gdx.utils.Array;
+
+public class CubeFinder {
+    public static final int CHUNK_SIZE = 8;
+    
+    
+    
+    public static void find2(Vector3d mapCenter, Quaternion mapRot, Cube3dMap map, Vector3d rayPos, Vector3d rayDir) {
+        Vector3d chunkSizes = new Vector3d(CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE);
+        Vector3d halfChunkSizes = chunkSizes.divide(2);
+        double radius = halfChunkSizes.length();
+        
+        Vector3d blockCenterOffset = halfChunkSizes.sub(map.getCenter3d());
+        
+        for(int x=0; x < map.xsize; x += CHUNK_SIZE) {
+            for(int y=0; y < map.ysize; y += CHUNK_SIZE) {
+                for(int z=0; z < map.zsize; z += CHUNK_SIZE) {
+                    Vector3d blockCenter = new Vector3d(x, y, z).add(blockCenterOffset).multiply(mapRot).add(mapCenter);
+                    
+                    boolean mh = checkBlock(blockCenter, radius, rayPos, rayDir);
+                }
+            }
+        }
+    }
+    
+    public static void find(Vector3d mapCenter, Quaternion mapRot, Cube3dMap map, Vector3d rayPos, Vector3d rayDir) {
+        Vector3d chunkSizes = new Vector3d(CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE);
+        Vector3d halfChunkSizes = chunkSizes.divide(2);
+        double radius = halfChunkSizes.length();
+
+        Vector3d mapOrigin = map.getCenter3d().negate().multiply(mapRot).add(mapCenter);
+
+        Vector3d xunit = Vector3d.UNIT_X.multiply(mapRot);
+        Vector3d yunit = Vector3d.UNIT_Y.multiply(mapRot);
+        Vector3d zunit = Vector3d.UNIT_Z.multiply(mapRot);
+        
+        Vector3d xunit2 = xunit.multiply(2);
+        Vector3d yunit2 = yunit.multiply(2);
+        Vector3d zunit2 = zunit.multiply(2);
+
+        Array<Vector3d> next = new Array<>();
+        for(int x=0; x < map.xsize; x += CHUNK_SIZE) {
+            for(int y=0; y < map.ysize; y += CHUNK_SIZE) {
+                for(int z=0; z < map.zsize; z += CHUNK_SIZE) {
+                    Vector3d dx = xunit.multiply(x + halfChunkSizes.x);
+                    Vector3d dy = yunit.multiply(y + halfChunkSizes.y);
+                    Vector3d dz = zunit.multiply(z + halfChunkSizes.z);
+                    
+                    Vector3d blockCenter = mapOrigin.add(dx).add(dy).add(dz);
+                    
+                    boolean mh = checkBlock(blockCenter, radius, rayPos, rayDir);
+                    if(mh) {
+                        next.add(blockCenter.add(xunit2).add(yunit2).add(zunit2));
+                        next.add(blockCenter.add(xunit2).add(yunit2).sub(zunit2));
+                        next.add(blockCenter.add(xunit2).sub(yunit2).add(zunit2));
+                        next.add(blockCenter.add(xunit2).sub(yunit2).sub(zunit2));
+                        next.add(blockCenter.sub(xunit2).add(yunit2).add(zunit2));
+                        next.add(blockCenter.sub(xunit2).add(yunit2).sub(zunit2));
+                        next.add(blockCenter.sub(xunit2).sub(yunit2).add(zunit2));
+                        next.add(blockCenter.sub(xunit2).sub(yunit2).sub(zunit2));
+                    }
+                }
+            }
+        }
+    }
+    
+    private static boolean checkBlock(Vector3d blockCenter, double blockRadius, Vector3d rayPos, Vector3d rayDir) {
+        double distance = blockCenter.distance(rayPos);
+        Vector3d rayPoint = rayPos.add(rayDir.multiply(distance));
+        double distance2 = rayPoint.distance(blockCenter);
+        return distance2 <= blockRadius;
+    }
+    
+}
