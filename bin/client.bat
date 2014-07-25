c4ad55e5f489abafe171f6e5f452c160d89e04d9
Teppo Koskinen <teppo.koskinen@kone.com>
2014-07-25 13:11:02 +0300

    mmm2

--- bin/client.bat
+++ bin/client.bat
@@ -1,5 +1,5 @@
 echo Fanfare for Space Kevin MacLeod (incompetech.com)
 echo All This Kevin MacLeod (incompetech.com)
 set JAVA_HOME="%~dp0jre7"
-%JAVA_HOME%\bin\javaw.exe -jar Megastage.jar -server localhost -player Developer -log-level 3 -gfx JOKE
+%JAVA_HOME%\bin\javaw.exe -jar Megastage.jar --server localhost --player Developer --log-level 2 --gfx JOKE
 
