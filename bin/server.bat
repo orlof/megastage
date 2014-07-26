2cb4b3d9fd5aca89cce2d48a8074112d764d5144
Orlof <orlof@mbnet.fi>
2014-07-27 00:39:53 +0300

    removed music copyright from server script

--- bin/server.bat
+++ bin/server.bat
@@ -1,4 +1,2 @@
-echo Fanfare for Space Kevin MacLeod (incompetech.com)
-echo All This Kevin MacLeod (incompetech.com)
 set JAVA_HOME="%~dp0jre7"
 %JAVA_HOME%\bin\javaw.exe -ea -cp Megastage.jar org.megastage.server.Main --config world.xml
--- bin/server.sh
+++ bin/server.sh
@@ -1,4 +1,1 @@
-echo 'Fanfare for Space Kevin MacLeod (incompetech.com)'
-echo 'All This Kevin MacLeod (incompetech.com)'
 java -ea -cp Megastage.jar org.megastage.server.Main --config world.xml
-
